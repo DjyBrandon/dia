5c703e900103c2ebbf38d5dfc77928443fc65af8
DjyBrandon <2903218578@qq.com>
2025-02-24 07:12:37 +0800

    simple bot initialization

---
+++ origin/simpleBot.py
@@ -0,0 +1,166 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Time    ：2025-02-23 19:51
+    @Project ：dia
+    @Author  ：Brandon Dong
+    @E-mail  : Brandon_Dong@outlook.com
+"""
+import tkinter as tk
+import random
+import math
+import numpy as np
+
+
+class Brain():
+    def __init__(self, bot_position):
+        self.bot = bot_position
+
+    def thinkAndAct(self):
+        speedLeft = 5
+        speedRight = 5
+        newX = None
+        newY = None
+        return speedLeft, speedRight, newX, newY
+
+
+class Bot():
+    def __init__(self, name_position):
+        self.name = name_position
+        self.x = random.randint(100, 900)
+        self.y = random.randint(100, 900)
+        self.theta = random.uniform(0.0, 2.0 * math.pi)  # [0, 2π]
+        self.ll = 60  # 车轴宽度
+        self.sl = 0.0
+        self.sr = 0.0
+
+    def setBrain(self, brain_position):
+        self.brain = brain_position
+
+    def draw(self, canvas):
+        points = [(self.x + 30 * math.sin(self.theta)) - 30 * math.sin((math.pi / 2.0) - self.theta),
+                  (self.y - 30 * math.cos(self.theta)) - 30 * math.cos((math.pi / 2.0) - self.theta),
+                  (self.x - 30 * math.sin(self.theta)) - 30 * math.sin((math.pi / 2.0) - self.theta),
+                  (self.y + 30 * math.cos(self.theta)) - 30 * math.cos((math.pi / 2.0) - self.theta),
+                  (self.x - 30 * math.sin(self.theta)) + 30 * math.sin((math.pi / 2.0) - self.theta),
+                  (self.y + 30 * math.cos(self.theta)) + 30 * math.cos((math.pi / 2.0) - self.theta),
+                  (self.x + 30 * math.sin(self.theta)) + 30 * math.sin((math.pi / 2.0) - self.theta),
+                  (self.y - 30 * math.cos(self.theta)) + 30 * math.cos((math.pi / 2.0) - self.theta)
+                  ]
+        canvas.create_polygon(points, fill="blue", tags=self.name)
+
+        self.sensorPositions = [(self.x + 20 * math.sin(self.theta)) + 30 * math.sin((math.pi / 2.0) - self.theta), \
+                                (self.y - 20 * math.cos(self.theta)) + 30 * math.cos((math.pi / 2.0) - self.theta), \
+                                (self.x - 20 * math.sin(self.theta)) + 30 * math.sin((math.pi / 2.0) - self.theta), \
+                                (self.y + 20 * math.cos(self.theta)) + 30 * math.cos((math.pi / 2.0) - self.theta) \
+                                ]
+
+        centre1PosX = self.x
+        centre1PosY = self.y
+        canvas.create_oval(centre1PosX - 8, centre1PosY - 8,
+                           centre1PosX + 8, centre1PosY + 8,
+                           fill="gold", tags=self.name)
+
+        wheel1PosX = self.x - 30 * math.sin(self.theta)
+        wheel1PosY = self.y + 30 * math.cos(self.theta)
+        canvas.create_oval(wheel1PosX - 3, wheel1PosY - 3,
+                           wheel1PosX + 3, wheel1PosY + 3,
+                           fill="red", tags=self.name)
+
+        wheel2PosX = self.x + 30 * math.sin(self.theta)
+        wheel2PosY = self.y - 30 * math.cos(self.theta)
+        canvas.create_oval(wheel2PosX - 3, wheel2PosY - 3,
+                           wheel2PosX + 3, wheel2PosY + 3,
+                           fill="green", tags=self.name)
+
+        sensor1PosX = self.sensorPositions[0]
+        sensor1PosY = self.sensorPositions[1]
+        sensor2PosX = self.sensorPositions[2]
+        sensor2PosY = self.sensorPositions[3]
+        canvas.create_oval(sensor1PosX - 3, sensor1PosY - 3,
+                           sensor1PosX + 3, sensor1PosY + 3,
+                           fill="yellow", tags=self.name)
+        canvas.create_oval(sensor2PosX - 3, sensor2PosY - 3,
+                           sensor2PosX + 3, sensor2PosY + 3,
+                           fill="yellow", tags=self.name)
+
+    def thinkAndAct(self):  # 根据脑子决定下一步去哪里
+        self.sl, self.sr, xx, yy = self.brain.thinkAndAct()
+        if xx != None:
+            self.x = xx
+        if yy != None:
+            self.y = yy
+
+    def update(self, canvas, dt):
+        self.move(canvas, dt)
+
+    def move(self, canvas, dt):
+        if self.sl == self.sr:
+            R = 0
+        else:
+            R = (self.ll / 2.0) * ((self.sr + self.sl) / (self.sl - self.sr))
+        omega = (self.sl - self.sr) / self.ll
+        ICCx = self.x - R * math.sin(self.theta)  # instantaneous centre of curvature
+        ICCy = self.y + R * math.cos(self.theta)
+        m = np.matrix([[math.cos(omega * dt), -math.sin(omega * dt), 0],
+                       [math.sin(omega * dt), math.cos(omega * dt), 0],
+                       [0, 0, 1]])
+        v1 = np.matrix([[self.x - ICCx], [self.y - ICCy], [self.theta]])
+        v2 = np.matrix([[ICCx], [ICCy], [omega * dt]])
+        newv = np.add(np.dot(m, v1), v2)
+        newX = newv.item(0)
+        newY = newv.item(1)
+        newTheta = newv.item(2)
+        newTheta = newTheta % (2.0 * math.pi)  # make sure angle doesn't go outside [0.0,2*pi)
+        self.x = newX
+        self.y = newY
+        self.theta = newTheta
+        if self.sl == self.sr:  # straight line movement
+            self.x += self.sr * math.cos(self.theta)  # sr wlog
+            self.y += self.sr * math.sin(self.theta)
+        canvas.delete(self.name)
+        self.draw(canvas)
+
+
+def initialise(window):
+    window.resizable(False, False)
+    canvas = tk.Canvas(window, width=1000, height=1000)
+    canvas.pack()  # 布局管理
+    return canvas
+
+
+def buttonClicked(x, y, agents):
+    for rr in agents:
+        if isinstance(rr, Bot):
+            rr.x = x
+            rr.y = y
+
+
+def createObjects(canvas, noOfBots):
+    agents = []
+    passiveObjects = []
+    for i in range(0, noOfBots):
+        bot = Bot("Bot" + str(i))
+        brain = Brain(bot)
+        bot.setBrain(brain)
+        agents.append(bot)
+        bot.draw(canvas)
+    canvas.bind("<Button-1>", lambda event: buttonClicked(event.x, event.y, agents))
+    return agents, passiveObjects
+
+
+def moveIt(canvas, agents, passiveObjects):
+    for rr in agents:
+        rr.thinkAndAct()
+        rr.update(canvas, 1.0)
+    canvas.after(50, moveIt, canvas, agents, passiveObjects)
+
+
+def main():
+    window = tk.Tk()
+    canvas = initialise(window)
+    agents, passiveObjects = createObjects(canvas, noOfBots=2)
+    moveIt(canvas, agents, passiveObjects)
+    window.mainloop()
+
+
+main()
